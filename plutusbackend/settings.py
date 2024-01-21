6f77c9d25328bfcbc7a22c39dd3606f142b59fff
kaushik200030 <f20190653@pilani.bits-pilani.ac.in>
2024-01-22 03:29:16 +0530

    updated settings

--- plutusbackend/settings.py
+++ plutusbackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@a3n=@y(s2ny#_)7$bj^94guzj27o02f@j5%^^wnqogyc%l1=+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
